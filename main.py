d69263507a90dc3b7caebb3d12d1dbc72da750a8
JuergenWetzel <juergen.wetzel2000@web.de>
2024-04-17 18:11:52 +0200

    Added resnet18 as model and changed last layer to 2 outputs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import torch
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+from torchvision.models import resnet18
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -15,3 +17,10 @@ train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
 print(len(train_loader))
+
+model = resnet18()
+for param in model.parameters():
+   param.requires_grad = False
+num_features = model.fc.in_features
+new_classification_layer = torch.nn.Linear(num_features, 2)
+model.fc = new_classification_layer
