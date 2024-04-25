890d099ad5bc0116311aa76c6d5073120dec9f58
JuergenWetzel <juergen.wetzel2000@web.de>
2024-04-25 15:15:09 +0200

    Possible to double amount of cat images

--- test.py
+++ test.py
@@ -6,46 +6,46 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, Subset, ConcatDataset
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from torchvision.models import resnet18, ResNet18_Weights
+from torchvision.models import resnet34, ResNet34_Weights
 
 class_mapping = [
     0,  # Class 0: Abyssinian - Cat
-    1,  # Class 1: American Bulldog - Dog
-    1,  # Class 2: American Pit Bull Terrier - Dog
-    1,  # Class 3: Basset Hound - Dog
-    1,  # Class 4: Beagle - Dog
+    1,  # Class 1: american Bulldog - Dog
+    1,  # Class 2: american Pit Bull Terrier - Dog
+    1,  # Class 3: basset Hound - Dog
+    1,  # Class 4: beagle - Dog
     0,  # Class 5: Bengal - Cat
     0,  # Class 6: Birman - Cat
     0,  # Class 7: Bombay - Cat
-    1,  # Class 8: Boxer - Dog
+    1,  # Class 8: boxer - Dog
     0,  # Class 9: British Shorthair - Cat
-    1,  # Class 10: Chihuahua - Dog
+    1,  # Class 10: chihuahua - Dog
     0,  # Class 11: Egyptian Mau - Cat
-    1,  # Class 12: English Cocker Spaniel - Dog
-    1,  # Class 13: English Setter - Dog
-    1,  # Class 14: German Shorthaired - Dog
-    1,  # Class 15: Great Pyrenees - Dog
-    1,  # Class 16: Havanese - Dog
-    1,  # Class 17: Japanese Chin - Dog
-    1,  # Class 18: Keeshond - Dog
-    0,  # Class 19: Leonberger - Cat
+    1,  # Class 12: english Cocker Spaniel - Dog
+    1,  # Class 13: english Setter - Dog
+    1,  # Class 14: german Shorthaired - Dog
+    1,  # Class 15: great Pyrenees - Dog
+    1,  # Class 16: havanese - Dog
+    1,  # Class 17: japanese Chin - Dog
+    1,  # Class 18: keeshond - Dog
+    1,  # Class 19: leonberger - Cat
     0,  # Class 20: Maine Coon - Cat
-    1,  # Class 21: Miniature Pinscher - Dog
-    1,  # Class 22: Newfoundland - Dog
+    1,  # Class 21: miniature Pinscher - Dog
+    1,  # Class 22: newfoundland - Dog
     0,  # Class 23: Persian - Cat
-    1,  # Class 24: Pomeranian - Dog
-    1,  # Class 25: Pug - Dog
+    1,  # Class 24: pomeranian - Dog
+    1,  # Class 25: pug - Dog
     0,  # Class 26: Ragdoll - Cat
     0,  # Class 27: Russian Blue - Cat
-    1,  # Class 28: Saint Bernard - Dog
-    1,  # Class 29: Samoyed - Dog
-    1,  # Class 30: Scottish Terrier - Dog
-    1,  # Class 31: Shiba Inu - Dog
+    1,  # Class 28: saint Bernard - Dog
+    1,  # Class 29: samoyed - Dog
+    1,  # Class 30: scottish Terrier - Dog
+    1,  # Class 31: shiba Inu - Dog
     0,  # Class 32: Siamese - Cat
     0,  # Class 33: Sphynx - Cat
-    1,  # Class 34: Staffordshire Bull Terrier - Dog
-    1,  # Class 35: Wheaten Terrier - Dog
-    1,  # Class 36: Yorkshire Terrier - Dog
+    1,  # Class 34: staffordshire Bull Terrier - Dog
+    1,  # Class 35: wheaten Terrier - Dog
+    1,  # Class 36: yorkshire Terrier - Dog
 ]
 
 transform = transforms.Compose([
@@ -61,10 +61,11 @@ def binary_transform(target):
 
 
 def load_data(test_size, batch_train, batch_test):
-
     train_set = datasets.OxfordIIITPet(root="Dataset", download=False, transform=transform, split="trainval", target_transform=binary_transform)
     test_set = datasets.OxfordIIITPet(root="Dataset", download=False, transform=transform, split="test", target_transform=binary_transform)
     dataset = ConcatDataset([train_set, test_set])
+#    cat_set = get_cats(dataset)
+#    dataset = ConcatDataset([dataset, cat_set])
     shuffled = list(range(len(dataset)))
     random.shuffle(shuffled)
     train_set = Subset(dataset, shuffled[test_size:])
@@ -74,13 +75,24 @@ def load_data(test_size, batch_train, batch_test):
     return trainloader, testloader
 
 
-train_loader, test_loader = load_data(1000, 100, 1000)
+def get_cats(dataset):
+    idxs = []
+    for i, data in enumerate(dataset):
+        image, label = data
+        if label == 0:
+            idxs.append(i)
+    print(len(idxs))
+    return Subset(dataset, idxs)
+
+
+
+train_loader, test_loader = load_data(1500, 100, 1500)
 
 
 class DCResNet18(nn.Module):
     def __init__(self):
         super().__init__()
-        self.network = resnet18(weights=ResNet18_Weights.DEFAULT)
+        self.network = resnet34(weights=ResNet34_Weights.DEFAULT)
         for param in self.network.parameters():
             param.requires_grad = False
         num_features = self.network.fc.in_features
@@ -95,7 +107,7 @@ class DCResNet18(nn.Module):
 path = "./"
 filename = "accuracy_long.csv"
 #with open(path + filename, "w") as file:
-#    file.write("weight, accuracy, loss, epochs, cat accuracy, dog accuracy\n")
+#    file.write("weight, accuracy, loss, epochs, amount training data, cat accuracy, dog accuracy\n")
 
 
 def accuracy(dataloader):
@@ -145,15 +157,21 @@ def train_model(model, dataloader, epochs=5, lr=0.001, weight=0.001):
     return model, loss.item()
 
 
-for weight in [0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.01, 0.02, 0.1, 0.2]:
+train_amount = 0
+for loader in train_loader:
+    train_amount += len(loader)
+
+
+for weight in [0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.01, 0.02, 0.1, 0.2]:
     print("Weight: %.2f" % weight)
-    epochs = 15
+    epochs = 7
     model = DCResNet18()
     model, loss = train_model(model, train_loader, weight=weight, epochs=epochs)
     print("accuracy training: " + str(100 * accuracy(train_loader)[0]) + "%")
     acc, accs = accuracy(test_loader)
     print("accuracy test: " + str(acc))
-    result = "%.5f,%.3f,%.5f,%d" % (weight, 100 * acc, loss, epochs)
+    
+    result = "%.5f,%.3f,%.5f,%d, %d" % (weight, 100 * acc, loss, epochs, train_amount)
     for acc in accs:
         acc = 100 * acc
         result += ", %.2f" % acc
